895657457800d80c246e0938d32d40d83c6f6c62
ulammatha <udaykumar1117@gmail.com>
2015-08-15 18:50:26 -0700

    minimum_rate_changes

---
+++ app/models/item.rb
@@ -0,0 +1,20 @@
+class Item < ActiveRecord::Base
+
+  CLEARANCE_PRICE_PERCENTAGE  = BigDecimal.new("0.75")
+  PANTS_AND_DRESSES = ["Pants", "Dress"]
+
+  belongs_to :style
+  belongs_to :clearance_batch
+
+  scope :sellable, -> { where(status: 'sellable') }
+
+  def clearance!
+    selling_price = style.wholesale_price * CLEARANCE_PRICE_PERCENTAGE
+    if selling_price < 5 && PANTS_AND_DRESSES.include?(style.type)
+      selling_price = 5
+    elsif selling_price < 2
+      selling_price = 2
+    end
+    update_attributes!(status: 'clearanced', price_sold: selling_price)
+  end
+end
