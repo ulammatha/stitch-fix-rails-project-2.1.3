33f6be38c9b2a5ce651c6c024bfdd531f8b044b9
ulammatha <udaykumar1117@gmail.com>
2015-08-19 15:32:50 -0700

    file name changes

--- spec/services/clearancing_service_spec.rb
+++ spec/services/clearancing_service_spec.rb
@@ -10,26 +10,30 @@ describe ClearancingService do
   end
 
   describe "#clearance_items!" do
-    it 'success clearance batch with item set to clearanced status' do
-      item_ids = items[0,5].map(&:id)
-      clearance_batch = clearancing_service.clearance_items!(item_ids)
-      expect(clearance_batch.persisted?).to be_truthy
-      expect(clearance_batch.items.pluck(:status)).to match_array([
-                                                        Item::STATUS[:clearanced],
-                                                        Item::STATUS[:clearanced],
-                                                        Item::STATUS[:clearanced],
-                                                        Item::STATUS[:clearanced],
-                                                        Item::STATUS[:clearanced],
-                                                        ])
+    context 'total success' do
+      it 'should create a clearance batch with items set to clearanced status' do
+        item_ids = items[0,5].map(&:id)
+        clearance_batch = clearancing_service.clearance_items!(item_ids)
+        expect(clearance_batch.persisted?).to be_truthy
+        expect(clearance_batch.items.pluck(:status)).to match_array([
+                                                          Item::STATUS[:clearanced],
+                                                          Item::STATUS[:clearanced],
+                                                          Item::STATUS[:clearanced],
+                                                          Item::STATUS[:clearanced],
+                                                          Item::STATUS[:clearanced],
+                                                          ])
+      end
+    end
+    context 'Failed celarance batch ' do
+      it 'should not create any clearance batch' do
+        item_ids = [" ", "test", 9999, 20.5, 0]
+        clearance_batch = clearancing_service.clearance_items!(item_ids)
+        expect(clearance_batch).to be_nil
+      end
     end
 
-    it 'Failed celarance batch ' do
-      item_ids = [" ", "test", 9999, 20.5, 0]
-      clearance_batch = clearancing_service.clearance_items!(item_ids)
-      expect(clearance_batch).to be_nil
-    end
-
-     it 'partial success celarance batch with item set to clearanced status ' do
+    context 'partial success ' do
+     it 'should create a celarance batch with partial items set to clearanced status ' do
       item_ids = items[0,2].map(&:id)
       item_ids << 9999
       clearance_batch = clearancing_service.clearance_items!(item_ids)
@@ -38,6 +42,7 @@ describe ClearancingService do
                                                         Item::STATUS[:clearanced],
                                                         Item::STATUS[:clearanced]
                                                         ])
+      end
     end
   end
 end
